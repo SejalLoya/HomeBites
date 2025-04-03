6b66d287c59a8c374519776bce2f5b7d6d0384e0
Sejal Loya <sejalloya9@gmail.com>
2025-04-03 22:11:31 +0530

    fixing CORS issue

--- server/index.js
+++ server/index.js
@@ -9,12 +9,14 @@ const routes = require("./routes");
 
 connectDb();
 app.use(express.json());
-app.use(cors(
-    {
-        origin: ['http://localhost:5173', 'https://home-bites-sepia.vercel.app/'],
+app.use(
+    cors({
+        origin: ['http://localhost:5173', 'https://home-bites-sepia.vercel.app'],
         credentials: true,
-    }
-));
+        methods: ["GET", "POST", "PUT", "DELETE"], // Allow necessary methods
+        allowedHeaders: ["Content-Type", "Authorization"], // Ensure correct headers
+    })
+);
 app.use(cookieParser());
 
 app.use("/api", routes);
