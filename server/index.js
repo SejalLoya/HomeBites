470acacc9bd3581743dd0d1167c954a4d08b9781
Sejal Loya <sejalloya9@gmail.com>
2025-04-03 22:07:23 +0530

    add vercel link to access frontend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ connectDb();
 app.use(express.json());
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://home-bites-sepia.vercel.app/'],
         credentials: true,
     }
 ));
